d30ea776ddb63b6a4427847dd6db38ce1ae2cd96
duol9 <lhyy0195@gmail.com>
2024-11-21 13:00:33 +0900

    feat : 햄버거 메뉴 선택 구현
    각 햄버거에 맞는 정보가 출력되도록 switch문 구현
    if문 대신 switch문을 사용해 가독성 높임

--- src/kiosk/Kiosk_Lv1/kiosk.java
+++ src/kiosk/Kiosk_Lv1/kiosk.java
@@ -22,6 +22,27 @@ public class kiosk {
             menuNum = sc.nextInt();
             sc.nextLine(); // 버퍼 비우기
 
+            //switch 문으로 조건문 활용
+            switch (menuNum) {
+                case 1:
+                    System.out.println("선택한 메뉴: ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("선택한 메뉴: SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.println("선택한 메뉴: CheeseBurger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                    System.out.println("");
+                    break;
+                case 4:
+                    System.out.println("선택한 메뉴: Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                    System.out.println("");
+                    break;
+                default:
+            }
+
         }  while (menuNum != 0);
 
     }
