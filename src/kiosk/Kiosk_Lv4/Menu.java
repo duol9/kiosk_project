d8ec0c35c25d2048b5dfc4a98a1f0f69075eea82
duol9 <lhyy0195@gmail.com>
2024-11-23 22:48:40 +0900

    refactor : Mune 클래스 카테고리명  필드 타입 수정 및 관련 메서드 삭제
    카테고리명 관리 필드(categoryNames) List<String>에서 String 타입으로 변경
     - 각 카테고리마다 객체를 생성하고 관리하도록 수정
    카테고리 이름을 출력하는 displayMenuCategory() 메서드 삭제
    - 기존 displayMenuCategory()는 카테고리명 필드 타입이 List였기에 가능한 로직이었으므로 삭제

--- src/kiosk/Kiosk_Lv4/Menu.java
+++ src/kiosk/Kiosk_Lv4/Menu.java
@@ -5,33 +5,15 @@ import java.util.List;
 // MenuItem 클레스를 관리
 public class Menu {
     //속성
-    private List<String> categoryNames; //카테고리 이름을 관리하는 리스트
+    private String categoryName; //카테고리 이름
     private List<MenuItem> menuItems; //메뉴를 관리하는 리스트
 
     //생성자
-    public Menu (List<String> categoryName, List<MenuItem> menuItems) {
-        this.categoryNames = categoryName;
+    public Menu (String categoryName, List<MenuItem> menuItems) {
+        this.categoryName = categoryName;
         this.menuItems = menuItems;
     }
     //기능
-    /**
-     * List에 들어있는 Category를 순차적으로 보여주는 메서드
-     * for문에서 categoryNames.get(i)으로 하나씩 카테고리명을 읽은 후 출력
-     */
-    public void displayMenuCategory() {
-
-        int categoryCount;  // 카테고리 개수
-        categoryCount = categoryNames.size(); // categoryNames에 담긴 카테고리 개수 저장
-
-        for (int i = 0; i<categoryCount; i++){
-            String categoryName = categoryNames.get(i);
-            System.out.printf("%-2d. %s%n",
-                    i+1, //번호 출력
-                    categoryName); //카테고리명 출력
-        }
-        System.out.println("0. 종료");
-    }
-
     /**
      * List에 들어있는 MenuItem을 순차적으로 보여주는 메서드
      * for문에서 menuItems.get(i)으로 하나씩 메뉴를 읽은 후 출력
@@ -54,7 +36,7 @@ public class Menu {
     /**
      * 메뉴 카테고리 이름을 외부로 반환
      */
-    public List<String> getCategoryName() {
-        return this.categoryNames;
+    public String getCategoryName() {
+        return this.categoryName;
     }
 }
