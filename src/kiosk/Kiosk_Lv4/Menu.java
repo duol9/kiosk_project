21c2e9fd95005eb5f18837f4e1ba09b2a0bbf7c8
duol9 <lhyy0195@gmail.com>
2024-11-23 22:07:25 +0900

    feat : Menu클래스 displayMenuItems() 구현
    메뉴를 출력하는 메서드 구현
    메뉴를 menuItems.get(i)로 순차적으로 얻은 후 출력
    동일한 포맷으로 출력될 수 있도록 정렬

--- src/kiosk/Kiosk_Lv4/Menu.java
+++ src/kiosk/Kiosk_Lv4/Menu.java
@@ -34,8 +34,21 @@ public class Menu {
 
     /**
      * List에 들어있는 MenuItem을 순차적으로 보여주는 메서드
+     * for문에서 menuItems.get(i)으로 하나씩 메뉴를 읽은 후 출력
      */
     public void displayMenuItems() {
+        int menuCount; // 메뉴의 개수
+        menuCount = menuItems.size(); // meuuItems에 담긴 메뉴 개수 저장
+
+        for(int i=0; i<menuCount; i++) {
+            MenuItem menuItem = menuItems.get(i);
+            System.out.printf("%-2d. %-12s | W %3.1f | %s%n",
+                    i+1, // 왼쪽 정렬하고 번호 출력
+                    menuItem.getName(), // 왼쪽 정렬하고 메뉴명 출력
+                    menuItem.getPrice(), // 오른쪽 정렬하고 가격 출력
+                    menuItem.getDescription()); // 설명 출력
+        }
+        System.out.println("0. 종료");
     }
 
     /**
