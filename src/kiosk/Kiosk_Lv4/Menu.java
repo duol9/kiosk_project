83f9873f0eb0fe9f1bdb39173966b1860fcc0004
duol9 <lhyy0195@gmail.com>
2024-11-23 22:05:30 +0900

    feat : Menu클래스 displayMenuCategory() 구현
    카테고리명을 categoryNames.get(i)로 순차적으로 얻은 후 출력
    동일한 포맷으로 출력될 수 있도록 정렬

--- src/kiosk/Kiosk_Lv4/Menu.java
+++ src/kiosk/Kiosk_Lv4/Menu.java
@@ -16,8 +16,20 @@ public class Menu {
     //기능
     /**
      * List에 들어있는 Category를 순차적으로 보여주는 메서드
+     * for문에서 categoryNames.get(i)으로 하나씩 카테고리명을 읽은 후 출력
      */
     public void displayMenuCategory() {
+
+        int categoryCount;
+        categoryCount = categoryNames.size();
+
+        for (int i = 0; i<categoryCount; i++){
+            String categoryName = categoryNames.get(i);
+            System.out.printf("%-2d. %s%n",
+                    i+1, //번호 출력
+                    categoryName); //카테고리명 출력
+        }
+        System.out.println("0. 종료");
     }
 
     /**
