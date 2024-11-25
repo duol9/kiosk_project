594369057ff18848fad577aa5ae8db039e67414c
duol9 <lhyy0195@gmail.com>
2024-11-25 15:24:20 +0900

    rename : DiscountType enum 추가 및 할인 기능 구현
    사용자 유형과 그에 대한 할인율 정의한 enum 상수 추가
    - String guestType, float discountRate
    할인 정보 출력 기능 추가
    - displayDiscountInfo ()
    할인 금액 계산 기능 추가
    - calcurateDiscountPrice()

---
+++ src/kiosk/Kiosk_Lv7/DiscountType.java
@@ -0,0 +1,51 @@
+package kiosk.Kiosk_Lv7;
+
+public enum DiscountType {
+    // enum 상수
+    NATIONAL_HERO("국가유공자", 0.10f),
+    SOLDIER("군인", 0.05f),
+    STUDENT("학생", 0.03f),
+    GENERAL("일반", 0.00f);
+
+    // enum 속성
+    private final String guestType;
+    private final float discountRate;
+
+    // enum 상수 호출 시 자동으로 초기화됨
+    DiscountType(String guestType, float discountRate) {
+        this.guestType = guestType;
+        this.discountRate = discountRate;
+    }
+
+    public String getGuestType() {
+        return this.guestType;
+    }
+
+    public float getDiscountRate() {
+        return this.discountRate;
+    }
+
+    /**
+     * 할인 정보 출력하는 메서드
+     * Enum 상수를 하나씩 가져와 사용자에게 손님 유형과 할인율을 정보를 제공
+     * enum.values() : enum상수를 리스트 형태로 접근 가능하게 함
+     */
+    public static void displayDiscountInfo () {
+        int i = 1;
+        for (DiscountType type : DiscountType.values()) {
+            System.out.printf("%d. %-10s : %.0f%%n",
+                    i++,
+                    type.getGuestType(),
+                    type.getDiscountRate() * 100);
+        }
+    }
+
+    /**
+     * 할인 적용된 금액 반환하는 메서드
+     * @param originalPrice (원래가격)
+     * @return 할인된 가격
+     */
+    public float calcurateDiscountPrice(float originalPrice) {
+        return originalPrice * (1 - discountRate);
+    }
+}
